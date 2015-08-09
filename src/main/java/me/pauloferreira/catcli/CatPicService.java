6d2583b231a4a4a88cab9f4927c0e6e244b85c73
Paulo Ferreira <paulo.bsf@gmail.com>
2015-08-09 18:50:17 +0100

    Add support for retrieving and storing cat photo

--- src/main/java/me/pauloferreira/catcli/CatPicService.java
+++ src/main/java/me/pauloferreira/catcli/CatPicService.java
@@ -1,34 +1,69 @@
 package me.pauloferreira.catcli;
 
+import com.squareup.okhttp.HttpUrl;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 public class CatPicService {
 
-  public static final String CAT_API_ENDPOINT = "http://thecatapi.com/api/images/get";
+  private static final String CAT_API_ENDPOINT = "http://thecatapi.com/api/images/get";
 
-  private final OkHttpClient okHttpClient;
+  private OkHttpClient okHttpClient;
 
   public CatPicService() {
     okHttpClient = new OkHttpClient();
   }
 
+  private Response executeHttpCall(String url, boolean followRedirects) throws IOException {
+    okHttpClient.setFollowRedirects(followRedirects);
+
+    Request request = new Request.Builder()
+        .url(url)
+        .build();
+
+    return okHttpClient.newCall(request).execute();
+  }
+
   public String getCatUrl() {
     try {
-      okHttpClient.setFollowRedirects(false);
-
-      Request request = new Request.Builder()
-          .url(CAT_API_ENDPOINT)
-          .build();
-
-      Response response = okHttpClient.newCall(request).execute();
+      Response response = executeHttpCall(CAT_API_ENDPOINT, false);
 
       return response.header("Location"); //  Assumes redirect
     } catch (IOException e) {
       throw new CatException("Could not retrieve cat url", e);
+    }
+  }
+
+  private String getFileName(HttpUrl httpUrl) {
+    List<String> pathSegments = httpUrl.pathSegments();
+
+    return pathSegments.get(pathSegments.size() - 1);
+  }
+
+  private String saveFileInDesktop(String fileName, byte[] content) throws IOException {
+    String filePath = System.getProperty("user.home") + "/Desktop/" + fileName;
+
+    FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+    fileOutputStream.write(content);
+    fileOutputStream.close();
+
+    return filePath;
+  }
+
+  public String getCatPhoto() {
+    try {
+      Response response = executeHttpCall(CAT_API_ENDPOINT, true);
+
+      HttpUrl redirectUrl = response.request().httpUrl();
+
+      return saveFileInDesktop(getFileName(redirectUrl), response.body().bytes());
+    } catch (IOException e) {
+      throw new CatException("Could not retrieve cat photo", e);
     }
   }
 }
--- src/test/java/me/pauloferreira/catcli/CatPicServiceTest.java
+++ src/test/java/me/pauloferreira/catcli/CatPicServiceTest.java
@@ -1,18 +1,91 @@
 package me.pauloferreira.catcli;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Protocol;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.squareup.okhttp.ResponseBody;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import java.net.URL;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.util.UUID;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CatPicServiceTest {
 
-  @Test
-  public void testGetCatUrl() throws Exception {
-    CatPicService catPicService = new CatPicService();
+  @Mock
+  private OkHttpClient okHttpClient;
 
-    //  implicitly checks for MalformedURLException (including null)
+  @Mock
+  private Call call;
+
+  @InjectMocks
+  private CatPicService catPicService = new CatPicService();
+
+  @Test
+  public void getCatUrl() throws Exception {
+    String expectedValue = "http://pauloferreira.me/cat.png";
+
+    Request request = new Request.Builder()
+        .url("http://pauloferreira.me/api/images/get")
+        .build();
+
+    Response response = new Response.Builder()
+        .request(request)
+        .protocol(Protocol.HTTP_1_1)
+        .code(302)
+        .addHeader("Location", expectedValue)
+        .build();
+
+    when(call.execute()).thenReturn(response);
+    when(okHttpClient.newCall(any())).thenReturn(call);
+
     String catUrl = catPicService.getCatUrl();
 
-    new URL(catUrl);
+    assertThat(catUrl, equalTo(expectedValue));
+  }
+
+  @Test
+  public void getCatPhoto() throws Exception {
+    String fileName = UUID.randomUUID().toString() + ".png";
+
+    String expectedValue = System.getProperty("user.home") + "/Desktop/" + fileName;
+
+    Request request = new Request.Builder()
+        .url("http://pauloferreira.me/" + fileName)
+        .build();
+
+    ResponseBody body = ResponseBody.create(MediaType.parse("image/png"), new byte[]{});
+
+    Response response = new Response.Builder()
+        .request(request)
+        .protocol(Protocol.HTTP_1_1)
+        .code(200)
+        .body(body)
+        .build();
+
+    when(call.execute()).thenReturn(response);
+    when(okHttpClient.newCall(any())).thenReturn(call);
+
+    String catPath = catPicService.getCatPhoto();
+
+    //  xxx: would be much better to mock fs somehow
+    Files.delete(FileSystems.getDefault().getPath(catPath));
+
+    assertThat(catPath, equalTo(expectedValue));
+
   }
 }
